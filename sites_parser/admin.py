27fb09f1defc5bf25a50bb4f90b51ea2caceeece
romanyakovlev <yakovlevroman8@gmail.com>
2019-07-12 20:41:02 +0700

    add model to admin

--- sites_parser/admin.py
+++ sites_parser/admin.py
@@ -4,11 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import SiteAdminModel
 
-# Register your models here.
+class SiteAdmin(admin.ModelAdmin):
 
-admin.site.register(SiteAdminModel)
+    list_display = ['site_url', 'timeshift_for_parsing']
+    readonly_fields = ('parsing_time', 'site_title', 'site_header', 'parsing_complete')
 
-class SiteAdminModel(admin.ModelAdmin):
-
-    list_display = ["site_url", "timeshift_for_parsing", "parsing_complete"]
-    readonly_fields = ('parsing_time', 'site_title', 'site_header')
+admin.site.register(SiteAdminModel, SiteAdmin)
--- sites_parser/models.py
+++ sites_parser/models.py
@@ -8,7 +8,7 @@ class SiteAdminModel(models.Model):
     site_url = models.URLField(blank=False)
     site_title = models.CharField(max_length=200, blank=True, null=True)
     site_header = models.CharField(max_length=200, blank=True, null=True)
-    pasring_complete = models.BooleanField(default=False)
+    parsing_complete = models.BooleanField(default=False)
     timeshift_for_parsing = models.PositiveIntegerField(default=0)
     parsing_time = models.DateTimeField(null=True)
 
