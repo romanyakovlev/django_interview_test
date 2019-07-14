f1d9fb982ce5dd5de5b1992bfa67866b7bbfbd8c
romanyakovlev <yakovlevroman8@gmail.com>
2019-07-15 01:09:16 +0700

    change site_coding to read only mode

--- sites_parser/admin.py
+++ sites_parser/admin.py
@@ -7,6 +7,6 @@ from .models import SiteAdminModel
 class SiteAdmin(admin.ModelAdmin):
 
     list_display = ['site_url', 'timeshift_for_parsing']
-    readonly_fields = ('parsing_time', 'site_title', 'site_header', 'parsing_complete')
+    readonly_fields = ('parsing_time', 'site_title', 'site_header', 'parsing_complete', 'site_encoding')
 
 admin.site.register(SiteAdminModel, SiteAdmin)
